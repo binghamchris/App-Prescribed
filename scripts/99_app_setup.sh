9e242424183da16311ec3846f61aefd18fe25fe3
Chris Bingham <chris@pearit.co.uk>
2015-06-14 15:19:51 +0200

    Update config file name
    Corrected a typo in the name of the admins.js config file name.
    Modified 99_app_setup.sh so that it gets the Node.js user name from the
    Elastic Beanstalk environment.

--- scripts/99_app_setup.sh
+++ scripts/99_app_setup.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # Post-deploy script for configuring the application after Elastic Beanstalk has deployed it
 
-# Find out where Elastic Beanstalk deployed the application to
+# Find out where Elastic Beanstalk deployed the application to and which user it runs under
 EB_APP_CURRENT_DIR=$(/opt/elasticbeanstalk/bin/get-config container -k app_deploy_dir)
+EB_APP_USER=$(/opt/elasticbeanstalk/bin/get-config container -k app_user)
 
 # Make sure the application is stopped
 su -s /bin/bash -c "cd $EB_APP_CURRENT_DIR; /opt/elasticbeanstalk/node-install/node-v0.10.26-linux-x64/bin/npm run-script unix-stop" $EB_APP_USER
@@ -11,8 +12,8 @@ su -s /bin/bash -c "cd $EB_APP_CURRENT_DIR; /opt/elasticbeanstalk/node-install/n
 # Run the setup script
 su -s /bin/bash -c "cd $EB_APP_CURRENT_DIR; /opt/elasticbeanstalk/node-install/node-v0.10.26-linux-x64/bin/npm run-script setup" $EB_APP_USER
 # Retrieve the configuration files from the Git repo
-wget -O /var/app/current/settings/admin.js https://raw.githubusercontent.com/binghamchris/App-Prescribed/master/config/admin.js
-chown $EB_APP_USER:$EB_APP_USER /var/app/current/settings/admin.js
+wget -O /var/app/current/settings/admins.js https://raw.githubusercontent.com/binghamchris/App-Prescribed/master/config/admins.js
+chown $EB_APP_USER:$EB_APP_USER /var/app/current/settings/admins.js
 wget -O /var/app/current/config.json https://raw.githubusercontent.com/binghamchris/App-Prescribed/master/config/config.json
 chown $EB_APP_USER:$EB_APP_USER /var/app/current/config.json
 
